539324306c371b17e334e5f66695c2f5a1d1d156
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-13 19:13:32 +0200

    Styling changes.

--- pages/board.tsx
+++ pages/board.tsx
@@ -3,6 +3,10 @@ import Board from "../components/Board";
 const BoardPage: React.FC = (): JSX.Element => {
 	return (
 		<main className="main board">
+			<header className="header">
+				<img src="/logo.png" alt="logo" className="logo" />
+				<h1 className="title">30 Seconds Game</h1>
+			</header>
 			<Board />
 		</main>
 	);
