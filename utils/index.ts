8861b6d6fee81030c3dd741a296b745ff215cd12
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-13 15:29:09 +0200

    Adds correct prod url.

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,4 @@
 export const BASE_URL =
-	process.env.NODE_ENV === "production" ? "" : "http://localhost:3000";
+	process.env.NODE_ENV === "production"
+		? "https://30-seconds-board.vercel.app/"
+		: "http://localhost:3000";
