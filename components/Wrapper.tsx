f68a8fa67a270716ae7293edc7c1412134cb66e0
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-13 17:20:30 +0200

    Adds provider.

--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -1,13 +1,16 @@
+import { AppProvider } from "../context/AppContext";
 import { WrapperProps } from "../interfaces";
 
 import Meta from "./Meta";
 
 const Wrapper: React.FC<WrapperProps> = ({ children }): JSX.Element => {
 	return (
-		<>
-			<Meta />
-			{children}
-		</>
+		<AppProvider>
+			<>
+				<Meta />
+				{children}
+			</>
+		</AppProvider>
 	);
 };
 
