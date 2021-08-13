3a2babd417c96ec3f08b450352a8247678f334c7
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-13 18:11:02 +0200

    Adds logic for changing position.

--- components/Board.tsx
+++ components/Board.tsx
@@ -2,9 +2,26 @@ import { useContext } from "react";
 
 import { AppContext } from "../context/AppContext";
 import { board } from "../data/board";
+import { Team } from "../interfaces";
 
 const Board: React.FC = (): JSX.Element => {
-	const { teams } = useContext(AppContext);
+	const { teams, setTeams } = useContext(AppContext);
+
+	const updateTeamCount = (type: "inc" | "dec", team: Team) => {
+		let newTeams: Team[] = [...teams];
+		for (let i = 0; i < teams.length; i++) {
+			if (teams[i].name === team.name) {
+				if (type === "inc") {
+					newTeams[i].boardPosition++;
+				} else {
+					newTeams[i].boardPosition--;
+				}
+			}
+		}
+
+		setTeams(newTeams);
+	};
+
 	return (
 		<section className="board">
 			{board.map((el, key) =>
@@ -31,6 +48,30 @@ const Board: React.FC = (): JSX.Element => {
 					</div>
 				)
 			)}
+
+			<div className="leaderboard">
+				<h2 className="subtitle">Leaderboard</h2>
+				{teams
+					.sort((a, b) => (a.boardPosition < b.boardPosition ? 1 : -1))
+					.map((team) => (
+						<div className="row">
+							<p className="name">{team.name}</p>
+							<p className="board-pos">{team.boardPosition}</p>
+							<button
+								className="up"
+								onClick={() => updateTeamCount("inc", team)}
+							>
+								<span>👆</span>
+							</button>
+							<button
+								className="down"
+								onClick={() => updateTeamCount("dec", team)}
+							>
+								<span>👇</span>
+							</button>
+						</div>
+					))}
+			</div>
 		</section>
 	);
 };
