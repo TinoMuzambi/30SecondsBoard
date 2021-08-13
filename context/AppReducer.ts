b95efa236368926850bfa1e1d5d5cb993ccfd7fa
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-13 17:18:06 +0200

    Updates reducer.

--- context/AppReducer.ts
+++ context/AppReducer.ts
@@ -2,31 +2,10 @@ import { State, Actions } from "../interfaces";
 
 const Reducer = (state: State, action: Actions): State => {
 	switch (action.type) {
-		case "UPDATE_SIGNED_IN":
+		case "SET_TEAMS":
 			return {
 				...state,
-				signedIn: action.signedIn,
-			};
-		case "SET_USER": {
-			return {
-				...state,
-				user: action.user,
-			};
-		}
-		case "SET_PROGRAMMES":
-			return {
-				...state,
-				programmes: action.programmes,
-			};
-		case "SET_MAJORS":
-			return {
-				...state,
-				majors: action.majors,
-			};
-		case "SET_COURSES":
-			return {
-				...state,
-				courses: action.courses,
+				teams: action.teams,
 			};
 	}
 };
