5c2139002c77603e17318f3ad8d9a6a0383d63a9
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-13 15:44:48 +0200

    Adds some board markup.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,26 @@
-const Home = () => (
-	<main>
-		<p>Clean Next.js with TypeScript and Sass Boilerplate</p>
-	</main>
-);
+const Home = () => {
+	function fillArray(value, len) {
+		if (len == 0) return [];
+		var a = [value];
+		while (a.length * 2 <= len) a = a.concat(a);
+		if (a.length < len) a = a.concat(a.slice(0, len - a.length));
+		return a;
+	}
+
+	const images = fillArray({ image: "https://unsplash.it/100" }, 63);
+	return (
+		<main className="main">
+			<h1 className="title">30 Seconds Game</h1>
+			<section className="board">
+				{images.map((el, key) => (
+					<div key={key}>
+						<img src={el.image} alt="Random" />
+					</div>
+				))}
+			</section>
+		</main>
+	);
+};
 
 export default Home;
+// 33
