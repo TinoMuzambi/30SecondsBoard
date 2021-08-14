29f1a92450c397717bb639cf607471ba1621dfb5
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-14 09:22:43 +0200

    Adds timer.

--- pages/board.tsx
+++ pages/board.tsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
+
 import Board from "../components/Board";
 
 const BoardPage: React.FC = (): JSX.Element => {
+	const [time, setTime] = useState(30);
+	const [start, setStart] = useState(false);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			if (time > 0 && start) {
+				setTime(time - 1);
+			}
+		}, 1000);
+		return () => clearTimeout(timer);
+	}, [start, time]);
+
+	const startTimer = () => {
+		setInterval(() => {
+			if (time > 0) {
+				setStart(true);
+			}
+		}, 1000);
+	};
+
 	return (
 		<main className="main board">
 			<header className="header">
 				<div className="wrapper">
 					<img src="/logo.png" alt="logo" className="logo" />
 					<div className="timer">
-						<h1 className="seconds">30</h1>
-						<button className="start">Start</button>
+						<h1 className="seconds">{time}</h1>
+						<button className="start" onClick={startTimer}>
+							Start
+						</button>
 					</div>
 				</div>
 				<h1 className="title">30 Seconds Game</h1>
