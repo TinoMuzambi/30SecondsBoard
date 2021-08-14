1545fc00d81d93bf3ad9caf01b1833c0f1947ade
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-14 09:08:11 +0200

    Styles button.

--- pages/board.tsx
+++ pages/board.tsx
@@ -4,7 +4,13 @@ const BoardPage: React.FC = (): JSX.Element => {
 	return (
 		<main className="main board">
 			<header className="header">
-				<img src="/logo.png" alt="logo" className="logo" />
+				<div className="wrapper">
+					<img src="/logo.png" alt="logo" className="logo" />
+					<div className="timer">
+						<h1 className="seconds">30</h1>
+						<button className="start">Start</button>
+					</div>
+				</div>
 				<h1 className="title">30 Seconds Game</h1>
 			</header>
 			<Board />
