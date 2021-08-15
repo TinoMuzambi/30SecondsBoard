a40b202500090f688936224ee141d8f58c0497cb
Tino Muzambi <tinomuzambi@gmail.com>
2021-08-15 22:20:57 +0200

    Adds comments.

--- utils/index.ts
+++ utils/index.ts
@@ -1,10 +1,12 @@
 import { Team } from "../interfaces";
 
+// Base url dependent on environment.
 export const BASE_URL =
 	process.env.NODE_ENV === "production"
 		? "https://30-seconds-board.vercel.app/"
 		: "http://localhost:3000";
 
+// Determine if given name is unique in teams array.
 export const isUnique = (name: string, teams: Team[]): boolean => {
 	const noOccurrences = teams.filter((team) => team.name === name);
 
