ea28e3e9514c926082ca5b2ecfd48b0e716fef41
Tinotenda Muzambi <tinomuzambi@gmail.com>
2026-09-08 04:58:26 +0200

    Add a restrictive content security policy (#2)

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: "default-src 'self'; base-uri 'self'; form-action 'self'; frame-ancestors 'none'; object-src 'none'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; font-src 'self'; connect-src 'self'; manifest-src 'self'; upgrade-insecure-requests",
+  },
   { key: "Referrer-Policy", value: "strict-origin-when-cross-origin" },
   { key: "X-Content-Type-Options", value: "nosniff" },
   { key: "X-Frame-Options", value: "DENY" },
